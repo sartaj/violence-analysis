d27368b412af59944bdab5255cc9f7082acf0e5a
Sartaj Chowdhury <sartaj@sartaj.me>
2016-10-24 05:19:30 -0500

    add disclaimer

--- src/components/Pages/Search/index.jsx
+++ src/components/Pages/Search/index.jsx
@@ -39,21 +39,47 @@ const subtitleStyle = {
   textTransform: 'uppercase'
 };
 
+const disclaimerStyle = {
+  boxShadow: '0 1px 2px rgba(0,0,0,0.3)',
+  // border: '2px dotted grey',
+  width: '100%',
+  margin: '2vh',
+  padding: '2vmin',
+  textAlign: 'center',
+  textTransform: 'uppercase',
+  background: "rgb(199, 84, 87)",
+  color: "white"
+};
+
+const linkStyle = {
+    color: 'white',
+    fontWeight: 'bold',
+    textDecoration: 'underline'
+};
+
 const Header = () =>
   <div style={headerStyle}>
     <div style={titleStyle}>The Violence Genome Project</div>
-    <div style={subtitleStyle}>Analyze and Edit a comprehensive open source dataset on terrorism</div>
+    <div style={subtitleStyle}>
+      The World's Most Comprehensive Data Initiative To Understand Violence
+    </div>
   </div>
 ;
-
+const Disclaimer = () =>
+  <div style={disclaimerStyle}>
+    This Project Is Currently <strong>In Beta</strong><br /><br />
+    Help Contribute To <a href="https://docs.google.com/spreadsheets/d/1ZnARV43ctifkpb5M9lu13w-VO9wTGhOF-2rKZBTwQDA/edit?usp=sharing" style={linkStyle}>data</a> and <a href="http://github.com/sartaj/violence-analysis" target="_blank" style={linkStyle}>source code</a>
+  </div>
+;
 const Filters = () => <div>&nbsp;</div>;
 const view = state => state.map(results =>
   <div>
     <Header />
     <div className="Filter-view" style={{ width: '30%', float: 'left'}}>
+      <Disclaimer />
       <Filters />
     </div>
-    <div className="Results-view" style={{ width: '70%', float: 'left'}}>
+    <div className="Results-view" style={{ width: '70%', float: 'left', paddingTop: '2vmin' }}>
       <Results results={results} />
     </div>
   </div>
